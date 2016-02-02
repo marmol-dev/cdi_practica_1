f73c51db537072d3b53d143d0eef746df6a62d35
TheMalvarez <malvarez.filgueira@gmail.com>
2016-02-02 14:21:19 +0100

    1.1.4 sin acabar

--- src/examplerunnable/Example.java
+++ src/examplerunnable/Example.java
@@ -1,12 +1,26 @@
 package examplerunnable;
 
+import java.util.Iterator;
+import java.util.Map;
+
+
 public class Example implements Runnable{
 	public void run(){
-		System.out.println("Hello world i am a java thread(runable)");	
+		System.out.println("Hello world i am a java thread(runable)");
 	}
 	
-	public static void main (String args[]){
+	public static void main (String args[]) throws InterruptedException{
+		Map hilos;
+		//Thread.sleep(1000);
 		(new Thread(new Example())).start();
+		hilos = Thread.getAllStackTraces();
+		Iterator<Thread> it = hilos.keySet().iterator();
+		Thread aux;
+		while(it.hasNext()){
+			aux = it.next();
+			System.out.println("ID: " + aux.getId() + ", Name: " + aux.getName());
+		}
 	}
 
 }
+
