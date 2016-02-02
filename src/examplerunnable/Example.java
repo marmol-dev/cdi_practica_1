6728fc06a8b94adc9dd31aab21eab23d18d860e0
Martin <tm07081995@gmail.com>
2016-02-02 13:56:08 +0100

    1.1.3

--- src/examplerunnable/Example.java
+++ src/examplerunnable/Example.java
@@ -1,11 +1,12 @@
 package examplerunnable;
 
-public class Example implements Runnable{
-	public void run(){
+public class Example implements Runnable {
+	public void run() {
 		System.out.println("Hello world i am a java thread(runable)");	
 	}
 	
-	public static void main (String args[]){
+	public static void main (String args[]) throws InterruptedException {
+		Thread.sleep(1000);
 		(new Thread(new Example())).start();
 	}
 
