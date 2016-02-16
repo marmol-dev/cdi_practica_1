4e8dbf1d88c4d99e8142e1af0a524eefb4239521
themalvarez <malvarez.filgueira@gmail.com>
2016-02-16 13:12:34 +0100

    v2.1

--- src/examplerunnable/Example.java
+++ src/examplerunnable/Example.java
@@ -5,20 +5,37 @@ import java.util.Map;
 
 
 public class Example implements Runnable{
+	
+	private int numHilo;
+	
+	public Example(int numHilo){
+		this.numHilo = numHilo;
+	}
+	
+	
+	
 	public void run(){
-		System.out.println("Hello world i am a java thread(runable)");
+		System.out.println("Hello, I'm thread number " + this.numHilo);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			return;
+		}
+		System.out.println("Bye, this was thread number " + this.numHilo);
 	}
 	
 	public static void main (String args[]) throws InterruptedException{
-		Map hilos;
-		Thread.sleep(1000);
-		(new Thread(new Example())).start();
-		hilos = Thread.getAllStackTraces();
-		Iterator<Thread> it = hilos.keySet().iterator();
-		Thread aux;
-		while(it.hasNext()){
-			aux = it.next();
-			System.out.println("ID: " + aux.getId() + ", Name: " + aux.getName());
+		int numHilos = 0;
+		
+		if(args.length == 1){
+			numHilos = Integer.parseInt(args[0]);
+		}else{
+			System.out.print("Numero incorrecto de parametros de entrada");
+		}
+		
+		for(int i = 0; i < numHilos; i++){
+			new Thread(new Example(i)).start();
 		}
 	}
 
